e78657f3551cba24752b007e3bccd0906e1cc8a3
stonkol <hector96fi@yahoo.es>
2023-12-30 23:24:19 +0100

    apudetu

--- noob-play/noobplay.playground/Contents.swift
+++ noob-play/noobplay.playground/Contents.swift
@@ -40,5 +40,22 @@ print(ages)
 
 // SET
 //var agesSet: Set<Int> = [] // init a set
+//unlike and array everyone is hashable and has an i dentifier, so if you want to search it will take the same time if the set has ten or ten million things inside
+// Set vs Array: unordered, no duplicates, good performance
 var agesSet = Set(ages)
 print(agesSet)
+
+agesSet.contains(41) //find if this set contains that number, return a boolean answer
+agesSet.insert(431) //insert
+
+
+
+
+// DICTIONARY
+//has labels, like in a dictionary you have a word and then a definition, here is key: value
+let devices_pedro: [String: String] = [:] //devices is the dictionary name, then it comes what will be the types(num, strings...), types the first String is the key(usually key are String), the second is the value, the [:] at the end is to create a empty dictionary
+let devices_jules: [String: String] = [
+    "phone": "Note 7",
+    "laptop": "Vaio 14",
+    
+]
